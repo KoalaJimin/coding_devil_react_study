504e173a54621ba88ee040c29864f2e432194013
Jimin <kgmline2@naver.com>
2024-07-13 14:01:52 +0900

    2024.07.13 컴포넌트 학습

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Hello1 from './component/Hello1';
+import Welcome from './component/Welcome';
+
+function App() {
+
+  return (
+    <div className="App">
+        <Hello1 />
+        <Hello1 />
+        <Welcome />
+    </div>
+  );
+}
+
+export default App;
